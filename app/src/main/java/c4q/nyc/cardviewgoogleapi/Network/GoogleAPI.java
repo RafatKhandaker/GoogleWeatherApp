64c1747b26aecae1c2a12cb4632fcd149c6b83b1
BlackJack <RedDragon@BlackJacks-MBP.home>
2016-11-03 18:27:51 -0400

    update

---
+++ app/src/main/java/c4q/nyc/cardviewgoogleapi/Network/GoogleAPI.java
@@ -0,0 +1,9 @@
+package c4q.nyc.cardviewgoogleapi.Network;
+
+/**
+ * Created by RedDragon on 11/3/16.
+ */
+
+public class GoogleAPI {
+    public static String Developer_key = token.key;
+}
---
+++ app/src/main/java/c4q/nyc/cardviewgoogleapi/Network/token.java
@@ -0,0 +1,9 @@
+package c4q.nyc.cardviewgoogleapi.Network;
+
+/**
+ * Created by RedDragon on 11/3/16.
+ */
+
+public class token {
+    public static String key = "";
+}
