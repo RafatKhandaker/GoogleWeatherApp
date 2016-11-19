96a512238a737a70dd6858f17d683deceaee2717
Josiel <jdlopez114@gmail.com>
2016-11-19 18:34:01 -0500

    final before switching

--- app/src/main/java/com/delaroystudios/CardView_GoogleApp/YouTubeCard.java
+++ app/src/main/java/com/delaroystudios/CardView_GoogleApp/YouTubeCard.java
@@ -1,6 +1,6 @@
 package com.delaroystudios.CardView_GoogleApp;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.delaroystudios.CardView_GoogleApp.GoogleNowCard;import com.google.android.youtube.player.YouTubeBaseActivity;
 
 public class YouTubeCard extends YouTubeBaseActivity implements GoogleNowCard {
 
